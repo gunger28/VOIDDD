405231f9b49d31c7350bbf1beeab3f4a43ebe2fe
BenzoGung <gungeranton@gmail.com>
2020-03-03 12:19:41 +0700

    fst

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 setTimeout(() => {
     document.querySelector("#mn").style.transform = "translateX(0)";
 }, 1000);
@@ -8,38 +9,68 @@ function txt(flag) {
   if (flag) {
     text.textContent = this.dataset.fullname;
 
-    text.classList.add('mouseEnter');
+    text.classList.add('mouseMoveOn');
   } else {
     text.textContent = this.dataset.shortname;
 
-    text.classList.remove('mouseEnter');
+    text.classList.remove('mouseMoveOn');
   }
 }
 
-function changeSource(src) {
+function changeStylesMenu() {
+  var menEL = document.getElementsByClassName("menu_element");
+  for( let i = 0; i < menEL.length; i++){ // проходим циклом по всем элементам массивоподобного объекта
+ //   menEL[i].style.color = "black"; // устанавливаем зеленый цвет текста каждому элементу
+ 
+ menEL[i].firstElementChild.style.color = "black";
+    //  menEL[i].classList.add('mouseEnter');
+    console.log(menEL.length +" " + menEL[i]);
+  }
+ 
+  
+  
+}
+
+function changeSource(src,element) {
+ 
     const video = document.getElementById("video");
     video.setAttribute("src", src);
-
+  
     video.onload = () => {
         video.play();
     };
+    
 }
+
+
 
 window.onload = function() {
     const menuElements = document.querySelectorAll(".menu_element");
+   
     const video = document.getElementById("video");
     video.volume = 0.1;
     
-    menuElements.forEach(menuElement => {
+
+    
+
+      menuElements.forEach(menuElement => {
         menuElement.addEventListener("click", function() {
-            changeSource(this.dataset.source);
+            changeSource(this.dataset.source,menuElements);
+            
+            changeStylesMenu();
         });
     });
+
+    
+    
+
+  
 
     menuElements.forEach(menuElement => {
       menuElement.addEventListener('mouseenter', event => txt.call(event.target, true));
     });
-
+    
+    
     menuElements.forEach(menuElement => {
       menuElement.addEventListener('mouseleave', event => txt.call(event.target, false));
     });
