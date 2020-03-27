107079117cc2a3ef4cae99cec48a5aad4524add7
Anton <ant>
2020-03-27 13:17:13 +0700

    gallery added but it's bullshit

--- scripts/index.js
+++ scripts/index.js
@@ -4,217 +4,209 @@ var flagSkills = false;
 var flagContacts = false;
 
 setTimeout(() => {
-  document.querySelector("#mn").style.transform = "translateX(0)";
-  // document.querySelector("#gal").style.transform = "translateY(0)";
+    document.querySelector("#mn").style.transform = "translateX(0)";
+    // document.querySelector("#gal").style.transform = "translateY(0)";
 }, 1000);
 
 
-function close_others(){
-if(flagGallery){
 
-  console.log("closed gal");
-  document.querySelector("#gal").style.transform = "rotateX(90deg)";
+function close_others() {
+    const open = true;
+    const close = false;
+    if (flagWork) {
+        work_move(close);
+        close_work_viewer();
+        flagWork = false;
+    }
+    if (flagGallery) {
+        gal_move(close);
+        flagGallery = false;
+    }
+    if (flagContacts) {
+        work_move(close);
+        flagContacts = false;
+    }
+    if (flagSkills) {
+        work_move(close);
+        flagSkills = false;
+    }
 }
-if(flagWork){
-
-  console.log("closed Works");
-  document.getElementById("works").style.left = "5100px";
-}
-if(flagSkills){
-
-
-  document.querySelector("#gal").style.transform = "rotateX(90deg)";
-}
-if(flagContacts){
-
-
-  document.querySelector("#gal").style.transform = "rotateX(90deg)";
-}
-
-
-
-}
-
 
 function gal_move(flag) {
-  if (flag) {
-    setTimeout(() => {
-      document.querySelector("#gal").style.transform = "rotateX(0deg)";
-    }, 500);
-flagGallery = true;
-  } else {
-    document.querySelector("#gal").style.transform = "rotateX(90deg)";
-    flagGallery = false
-  }
+    const gallery = document.querySelector("#gal");
+
+    if (flag) {
+        close_others();
+        setTimeout(() => {
+            gallery.style.transform = "rotateX(0deg)";
+           
+        }, 500);
+
+        
+        flagGallery = true;
+    } else {
+        
+        gallery.style.transform = "rotateX(90deg)";
+    }
 }
 
-function view_move(obj) {
+function gal_view_move(obj) {
   const viewer = document.getElementById("viewer");
-  console.log("work");
+  console.log("work Viever opened");
   setTimeout(() => {
-    viewer.style.transform = "rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)" ;
-    take_pic_view(obj);
+      viewer.style.transform = "rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)";
+      take_pic_view(obj);
   }, 700);
-  
-  viewer.style.transform = "rotateX(90deg) rotateY(180deg) scale3d(0.5, 0.5, 0.5) ";
+
+  viewer.style.transform =
+      "rotateX(90deg) rotateY(180deg) scale3d(0.5, 0.5, 0.5) ";
+}
+
+function work_move(flag) {
+    const works = document.getElementById("works");
+
+    if (flag) {
+        close_others();
+        setTimeout(() => {
+            works.style.left = "100px";
+            
+            changeStyles_Black();
+        }, 1000);
+
+        flagWork = true;
+        console.log("work opened!");
+    } else {
+      console.log("work closed!");
+        works.style.left = "2100px";
+    }
 }
 
 
-function load_Works(flag){
-  if(flag){
-setTimeout(() => {
-  document.getElementById("works").style.left = "100px";
-  console.log("change left");
-  changeStyles_Black();
-flagWork = true;
-}, 1000);
-}else{
-  document.getElementById("works").style.left = "5100px";
-flagWork = false;
-}
-
-
-
-}
-// const works = document.getElementById("works").style.left = "100";
-// works.addEventListener("click", function(){
-
-// load_Works()
-
-// });
 
 function txt(flag) {
-  const text = this.firstElementChild;
+    const text = this.firstElementChild;
 
-  if (flag) {
-    text.textContent = this.dataset.fullname;
+    if (flag) {
+        text.textContent = this.dataset.fullname;
 
-    text.classList.add("mouseMoveOn");
-  } else {
-    text.textContent = this.dataset.shortname;
+        text.classList.add("mouseMoveOn");
+    } else {
+        text.textContent = this.dataset.shortname;
 
-    text.classList.remove("mouseMoveOn");
-  }
+        text.classList.remove("mouseMoveOn");
+    }
 }
 
 function changeStyles_White() {
-  var menEL = document.getElementsByClassName("menu_element");
-  for (let i = 0; i < menEL.length; i++) {
-    // проходим циклом по всем элементам массивоподобного объекта
-    //   menEL[i].style.color = "black"; // устанавливаем зеленый цвет текста каждому элементу
+    var menEL = document.getElementsByClassName("menu_element");
+    for (let i = 0; i < menEL.length; i++) {
+        // проходим циклом по всем элементам массивоподобного объекта
 
-    menEL[i].firstElementChild.style.color = "black";
-    //  menEL[i].classList.add('mouseEnter');
-    console.log(menEL.length + " " + menEL[i]);
-  }
+        menEL[i].firstElementChild.style.color = "black";
+    }
 }
 
 function changeStyles_Black() {
-  var menEL = document.getElementsByClassName("menu_element");
-  for (let i = 0; i < menEL.length; i++) {
-    
-    menEL[i].firstElementChild.style.color = null;
-    menEL[i].firstElementChild.removeAttribute("style");
-   
-    console.log(menEL.length + " " + menEL[i]);
-  }
+    var menEL = document.getElementsByClassName("menu_element");
+    for (let i = 0; i < menEL.length; i++) {
+        menEL[i].firstElementChild.style.color = null;
+        menEL[i].firstElementChild.removeAttribute("style");
+
+        console.log(menEL.length + " " + menEL[i]);
+    }
 }
+
+const video = document.getElementById("video");
+const backPane = document.getElementsByClassName("vid_block");
 
 function changeSource(src, element) {
-  const video = document.getElementById("video");
-  //video.style.backgroundImage = "./assets/gallery/logoSite.png";
-  if(element.dataset.fullname === "Main" ){
-    close_others();
-    load_Works(true);
-console.log(flagGallery);
-video.setAttribute("src", src);
-  video.onload = () => {
-    video.play();
-  };
+    //video.style.backgroundImage = "./assets/gallery/logoSite.png";
+    if (element.dataset.fullname === "Main") {
+        video.setAttribute("src", src);
+        video.onload = () => {
+            video.play();
+        };
 
-  }
+        changeStyles_Black();
+        video.style.opacity = "1";
+    }
 
-  if(element.dataset.fullname === "Gallery" ){
-    close_others();
-    gal_move(true);
-    video.setAttribute("src", src);
-  video.onload = () => {
-    video.play();
-  };
+    if (element.dataset.fullname === "Gallery") {
+        // close_others();
+        gal_move(true);
+        video.setAttribute("src", src);
+        video.onload = () => {
+            video.play();
+        };
 
-  }
-  
+        flagGallery = true;
+        changeStyles_White();
+        video.style.opacity = "0";
+    }
 
-  if(element.dataset.fullname === "Works" ){
-    close_others();
-    load_Works(true);
-console.log(flagGallery);
- video.setAttribute("src", src);
-  video.onload = () => {
-  video.play();
-  };
+    if (element.dataset.fullname === "Works") {
+        work_move(true);
 
-  }
+        video.setAttribute("src", src);
+        video.onload = () => {
+            video.play();
+            S;
+        };
+
+        
+        changeStyles_White();
+        video.style.opacity = "0";
+    }
 }
 
-
 window.onload = function() {
-  
-
-
-  const prews = document.querySelectorAll(".prew");
-  prews.forEach(prew => {
-    prew.addEventListener("click", function(){
-    view_move(event.target.getAttribute("id"))
+    const prews = document.querySelectorAll(".prew");
+    prews.forEach(prew => {
+        prew.addEventListener("click", function() {
+            gal_view_move(event.target.getAttribute("id"));
+        });
     });
-  });
 
+    const menuElements = document.querySelectorAll(".menu_element");
+    const mainVid = document.getElementById("main_video");
+    const video = document.getElementById("video");
+    video.volume = 0.1;
 
+    menuElements.forEach(menuElement => {
+        menuElement.addEventListener("click", function() {
+            changeSource(this.dataset.source, this);
+            //close_others();
+        });
+    });
 
-
-  const menuElements = document.querySelectorAll(".menu_element");
-  const mainVid = document.getElementById("main_video");
-  const video = document.getElementById("video");
-  video.volume = 0.1;
-
-  menuElements.forEach(menuElement => {
-    menuElement.addEventListener("click", function() {
-      changeSource(this.dataset.source, this);
+    mainVid.addEventListener("click", function() {
       close_others();
-      changeStyles_White();
+        changeSource(mainVid.dataset.source, mainVid);
     });
-  });
 
-  mainVid.addEventListener("click", function() {
-    changeSource(mainVid.dataset.source, mainVid);
-    close_others();
-    changeStyles_Black();
-  });
+    const category = document.querySelectorAll(".cat");
+    category.forEach(categor => {
+        categor.addEventListener("click", event =>
+            reload_imgs(event.target.getAttribute("id"))
+        );
+    });
 
+    menuElements.forEach(menuElement => {
+        menuElement.addEventListener("mouseenter", event =>
+            txt.call(event.target, true)
+        );
+    });
 
-  const category = document.querySelectorAll(".cat");
-  category.forEach(categor => {
-    categor.addEventListener("click", event =>
-      reload_imgs(event.target.getAttribute("id"))
-    );
-  });
+    menuElements.forEach(menuElement => {
+        menuElement.addEventListener("mouseleave", event =>
+            txt.call(event.target, false)
+        );
+    });
 
-  menuElements.forEach(menuElement => {
-    menuElement.addEventListener("mouseenter", event =>
-      txt.call(event.target, true)
-    );
-  });
+    tagsWorksFonts.forEach(text => {
+        // console.log(text.firstElementChild);
+        text.firstElementChild.style = "color: white; font-size: 65px;";
+    });
 
-  menuElements.forEach(menuElement => {
-    menuElement.addEventListener("mouseleave", event =>
-      txt.call(event.target, false)
-    );
-  });
-
-  tagsWorksFonts.forEach(text => {
-   // console.log(text.firstElementChild);
-    text.firstElementChild.style = "color: white; font-size: 65px;";
-  });
-  
-
+    this.opacity_img(true);
 };
--- scripts/work.js
+++ scripts/work.js
@@ -1,185 +1,183 @@
 var lastWork;
 
 //const name = ["cosmos" ,"death","merry","dright","funeral","plane","opposing","funeral","funeral","funeral","funeral","funeral"];
-const description = ["cosmos good","funeral bad","funeral bad"];
-const tecnology = ["cycle","eevee","eevee"];
-const styles = ["loft","groft"];
-const program = ["blender","gimp"];
+const description = ["jjkkk", "funeral bad", "funeral bad"];
+const tecnology = ["eevee", "cycles","cycles", "cycles","eevee","eevee","eevee","cycles","eevee","eevee","eevee","eevee","eevee"];
+const styles = ["loft", "groft"];
+const program = ["blender", "gimp"];
 
 var fSSize = "40px;";
 var fBSize = "65px;";
 
-
 var boolWork = false;
+var boolOpacity = true;
 
 const tagsWorksFonts = document.querySelectorAll(".tag");
 
 const cards = document.querySelectorAll(".one_card");
 
-function setWork(work){
-const title = document.querySelectorAll(".title");
-const img = document.querySelectorAll(".img_block");
-const desc = document.querySelectorAll(".text");
-const tecs = document.querySelectorAll(".tecnologies");
-const style = document.querySelectorAll(".style");
-const progs = document.querySelectorAll(".programs");
+function setWork(work) {
+  const title = document.querySelectorAll(".title");
+  const img = document.querySelectorAll(".img_block");
+  const desc = document.querySelectorAll(".text");
+  const tecs = document.querySelectorAll(".tecnologies");
+  const style = document.querySelectorAll(".style");
+  const progs = document.querySelectorAll(".programs");
 
+  title[0].textContent = work.getAttribute("alt");
+  img[0].childNodes[1].src = work.src;
+  desc[0].childNodes[1].textContent = description[work.dataset.source];
+  tecs[0].textContent = "Tecnologies: " + tecnology[work.dataset.source];
+  style[0].textContent = "Style: " + styles[work.dataset.source];
 
-title[0].textContent = work.getAttribute("alt");
-img[0].childNodes[1].src = work.src;
-desc[0].childNodes[1].textContent = description[work.dataset.source];
-tecs[0].textContent = "Tecnologies: " + tecnology[work.dataset.source];
-style[0].textContent = "Style: " + styles[work.dataset.source];
+  oneProg = "<p>" + program[0] + "</p>";
+  secProg = "<p>" + program[1] + "</p>";
+  progs[0].innerHTML = oneProg;
+  progs[0].innerHTML += secProg;
+}
 
-oneProg = '<p>'+ program[0]+'</p>';
-secProg = '<p>'+ program[1]+'</p>';
-progs[0].innerHTML = oneProg;
-progs[0].innerHTML += secProg;
+cards.forEach(card => {
+  card.addEventListener(
+    "click",
+    event => preShow(event.target) // event.target это уже img block
+  );
+});
 
+cards.forEach(card => {
+  card.addEventListener("mouseenter", function() {
+    ///console.log("little size black"), //console.log(card.childNodes),
+    // card.style.opacity = "1",
+    card.childNodes[1].childNodes[1].style =
+      "color: black; font-Size:" + fSSize + ";";
+    // card.childNodes[1].childNodes[1].style.backgroundColor = "red",
+    //card.childNodes[1].childNodes[1].style.fontSize = "40px",
+    // card.children.children.textContent = "ererer",
+    console.log(card.children[1]);
+    //card.style.opacity = "1";
+    if (lastWork != null && boolWork === true) {
+      lastWork.style.opacity = "1";
+    }
+
+    card.children[1].style.opacity = "1";
+  });
+
+  card.addEventListener("mouseleave", function() {
+    opacity_img(boolOpacity);
+    reverseToWhite(card);
+    if (lastWork != null && boolWork === true) {
+        lastWork.style.opacity = "1";
+      }
+  });
+});
+
+function reverseToWhite(elem) {
+  if (!boolWork) {
+    //console.log(card.childNodes[1].childNodes[1]),
+
+    elem.childNodes[1].childNodes[1].style =
+      "color: white; font-Size:" + fBSize;
+  } else {
+    elem.childNodes[1].childNodes[1].style =
+      "color: white; font-Size:" + fSSize;
+  }
+}
+
+function opacity_img(flag) {
+  cards.forEach(card => {
+    if (flag) {
+      card.children[1].style.opacity = "0";
+    }
+  });
+}
+
+const descPane = document.getElementById("work");
+function preShow(work) {
+  // console.log(work)
+  //console.log(work),
+  showWork(boolWork, work);
+}
+
+function close_work_viewer(){
+    const textWorks = document.querySelectorAll(".tag");
+    const workPane = document.getElementById("work");
+    const worksPane = document.getElementById("works");
+
+    worksPane.style.width = "100vw";
+    workPane.style.right = "-500px";
+    boolOpacity = true;
+    boolWork = false;
+    cards.forEach(card => {
+        card.children[1].style.opacity = "0";
+       // console.log(card.children[1]);
+    });
+    textWorks.forEach(text => {
+        text.childNodes[1].style = "color: white; font-Size: 65px;";
+      });
 }
 
 
-cards.forEach(card => {
-    card.addEventListener("click", event =>
+var switcherVar;
 
-  preShow(event.target) // event.target это уже img block
-   
-   
-    );
-  });
+function showWork(flag, work) {
+  const workPane = document.getElementById("work");
+  const worksPane = document.getElementById("works");
+  const textWorks = document.querySelectorAll(".tag");
 
-  cards.forEach(card => {
-    card.addEventListener("mouseenter", event =>
-//console.log(card),
-   //console.log(card.childNodes[1].childNodes[1]),
-   card.style.opacity = "1",
-   card.childNodes[1].childNodes[1].style = "color: black; font-Size:" + fSSize
-   
-    );
-
-    card.addEventListener("mouseleave", event =>
-    reverseToWhite(card)
-    );
-    
-  
-  });
-
-  function reverseToWhite(elem){
-
-    if(!boolWork){
-        //console.log(card.childNodes[1].childNodes[1]),
-        elem.childNodes[1].childNodes[1].style = "color: white; font-Size:" + fBSize
-         }else{
-
-            elem.childNodes[1].childNodes[1].style = "color: white; font-Size:" + fSSize
-         }
-
-  }
- 
-
-  const descPane = document.getElementById("work");
-  function preShow(work){
-   // console.log(work)
-   console.log(work),
-       showWork(boolWork,work);
-  
-
-  };
-
-  var switcherVar;
- 
-
-  function showWork(flag,work){
-
-    const workPane = document.getElementById("work");
-    const worksPane = document.getElementById("works");
-    const textWorks = document.querySelectorAll(".tag");
-    
-
-
-
-console.log(work.parentNode);
-   if(!flag){
-
-    
-       worksPane.style.width = "65vw";
-workPane.style.right = "0";
-setWork(work);
-        textWorks.forEach(text => {
-            text.childNodes[1].style = "color: white; font-Size: 40px;"
-            
-            boolWork = true;
-            switcherVar = work.getAttribute("alt");
-            work.style.opacity = "1";
-          });
-        }else{
-           if(switcherVar === work.getAttribute("alt")){
-            worksPane.style.width = "100vw";
-            workPane.style.right = "-500px";
-            work.style.opacity = "0";
-
-            textWorks.forEach(text => {
-                text.childNodes[1].style = "color: white; font-Size: 65px;"
-              });
-             
-             // switcherVar = work.getAttribute("alt");
-console.log("same card");
-
-              boolWork = false;
-
-             
-           }else{
-            console.log("NO same card");
-
-            textWorks.forEach(text => {
-              
-              });
-  work.style.opacity = "1";
-            workPane.style.right = "-500px";
-
-                textWorks.forEach(text => {
-                  //  text.childNodes[1].style = "color: white; font-Size: 65px;"
-                  });
-                 
-    
-                  setTimeout(() => {
-                    worksPane.style.width = "65vw";
+  //console.log(work.parentNode);
+  if (!flag) {
+    worksPane.style.width = "65vw";
     workPane.style.right = "0";
     setWork(work);
+    textWorks.forEach(text => {
+      text.childNodes[1].style = "color: white; font-Size: 40px;";
 
-            textWorks.forEach(text => {
-                text.childNodes[1].style = "color: white; font-Size: 40px;"
-                
-               // boolWork = true;
-               
-              });
-                  }, 1000);
-                  switcherVar = work.getAttribute("alt");
-    
-                  lastWork.style.opacity = "0";
-                  
+     // boolOpacity = false;
+      boolWork = true;
+      switcherVar = work.getAttribute("alt");
+      work.style.opacity = "1";
+    });
+  } else {
+    if (switcherVar === work.getAttribute("alt")) {
+      worksPane.style.width = "100vw";
+      workPane.style.right = "-500px";
+      work.style.opacity = "0";
 
-           }
-              
-                 
+      textWorks.forEach(text => {
+        text.childNodes[1].style = "color: white; font-Size: 65px;";
+      });
 
-          
+      // switcherVar = work.getAttribute("alt");
+      console.log("same card");
 
-                
+      boolOpacity = true;
+      boolWork = false;
+    } else {
+      console.log("NO same card");
+    //  boolOpacity = false;
+      textWorks.forEach(text => {});
+      work.style.opacity = "1";
+      workPane.style.right = "-500px";
 
+      textWorks.forEach(text => {
+        //  text.childNodes[1].style = "color: white; font-Size: 65px;"
+      });
 
-            
+      setTimeout(() => {
+        worksPane.style.width = "65vw";
+        workPane.style.right = "0";
+        setWork(work);
 
-      
+        textWorks.forEach(text => {
+          text.childNodes[1].style = "color: white; font-Size: 40px;";
 
+          // boolWork = true;
+        });
+      }, 1000);
+      switcherVar = work.getAttribute("alt");
 
-
-
-        }
-
-        lastWork = work;
-
+      lastWork.style.opacity = "0";
+    }
   }
-  
 
- 
+  lastWork = work;
+}
