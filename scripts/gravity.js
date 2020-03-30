904a3cebd5bd1f3be43240961c6f550c9e4b503f
Anton <ant>
2020-03-30 13:58:52 +0700

    gallery added but it's bullshit

--- scripts/gravity.js
+++ scripts/gravity.js
@@ -5,12 +5,18 @@ ctx = canv.getContext('2d');
 //ctx.fillStyle = 'red';
 //ctx.fillRect(20,20,10,2);
 
-w = canv.width;
-h = canv.height;
+//w = canv.width;
+//h = canv.height;
+
+const w = canv.getBoundingClientRect().width;
+const h = canv.getBoundingClientRect().height;
+
+canv.width = w/1.3;
+canv.height = h*4;
 
 console.log(w,h);
 ctx.beginPath();
-ctx.arc(150, 75, 50, 0, 2*Math.PI, false);
+ctx.arc(55, 75, 50, 0, 2*Math.PI, false);
 ctx.fillStyle = 'black';
 ctx.fill();
 ctx.lineWidth = 1;
